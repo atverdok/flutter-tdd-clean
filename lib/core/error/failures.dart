da718f9b1d39e7c983b8c536a2ec12d2274cb35d
atverdok <lexa_dpp@ukr.net>
2020-07-08 15:43:26 +0300

    5 Contracts of Data Sources

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -1,5 +1,11 @@
 import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {
-  Failure([List properties = const <dynamic>[]]);
+  @override
+  List<Object> get props => [];
 }
+
+// Genaral failures
+class ServerFailure extends Failure {}
+
+class CashFailure extends Failure {}
